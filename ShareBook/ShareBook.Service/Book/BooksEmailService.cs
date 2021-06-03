1f235793d0297c1b5bb10f6f6e07d0c5008c0e85
Raffaello Damgaard <raffacabofrio@gmail.com>
2021-06-03 17:47:03 -0300

    Formatar a data no email "novo livro cadastrado".

--- ShareBook/ShareBook.Service/Book/BooksEmailService.cs
+++ ShareBook/ShareBook.Service/Book/BooksEmailService.cs
@@ -1,15 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore.Internal;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
-using Org.BouncyCastle.Math.EC.Rfc7748;
 using ShareBook.Domain;
 using ShareBook.Domain.DTOs;
-using ShareBook.Service.AWSSQS;
-using ShareBook.Service.AWSSQS.Dto;
 using ShareBook.Service.Server;
-using System;
 using System.Linq;
-using System.Security.Cryptography;
 using System.Threading.Tasks;
 
 namespace ShareBook.Service
@@ -17,9 +11,9 @@ namespace ShareBook.Service
     public class BooksEmailService : IBooksEmailService
     {
         private const string NewBookInsertedTemplate = "NewBookInsertedTemplate";
-        private const string NewBookInsertedTitle = "Novo livro incluído - Sharebook";
+        private const string NewBookInsertedTitle = "Novo livro incluído!";
         private const string WaitingApprovalTemplate = "WaitingApprovalTemplate";
-        private const string WaitingApprovalTitle = "Aguarde aprovação do livro - Sharebook";
+        private const string WaitingApprovalTitle = "Aguarde aprovação do livro.";
         private const string BookApprovedTemplate = "BookApprovedTemplate";
         private const string BookApprovedTitle = "Livro aprovado - Sharebook";
         private const string NewBookNotifyTemplate = "NewBookNotifyTemplate";
@@ -28,7 +22,6 @@ namespace ShareBook.Service
         private readonly IEmailService _emailService;
         private readonly IUserService _userService;
         private readonly IEmailTemplate _emailTemplate;
-        private readonly IAWSSQSService _AWSSQSService;
         private readonly ServerSettings _serverSettings;
         private readonly IConfiguration _configuration;
 
@@ -36,14 +29,12 @@ namespace ShareBook.Service
             IEmailService emailService,
             IUserService userService,
             IEmailTemplate emailTemplate,
-            IAWSSQSService AWSSQSService,
             IOptions<ServerSettings> serverSettings,
             IConfiguration configuration)
         {
             _emailService = emailService;
             _userService = userService;
             _emailTemplate = emailTemplate;
-            _AWSSQSService = AWSSQSService;
             _serverSettings = serverSettings.Value;
             _configuration = configuration;
         }
@@ -87,36 +78,38 @@ namespace ShareBook.Service
 
         public async Task SendEmailBookToInterestedUsers(Book book)
         {
-            int MAX_DESTINATIONS = int.Parse(_configuration["AWSSQSSettings:MaxDestinationsPerMessage"]);
+            // TODO: reformular o mailling new book
+            
+            //int MAX_DESTINATIONS = int.Parse(_configuration["AWSSQSSettings:MaxDestinationsPerMessage"]);
 
-            var vm = new
-                {
-                    Book = book,
-                    ServerSettings = _serverSettings,
-                    name = "{name}"// recebe {name} pois e o padrao que o servico Consumer reconhece para substituicao para o nome do usuario para o qual ira enviar o email
-                };
+            //var vm = new
+            //    {
+            //        Book = book,
+            //        ServerSettings = _serverSettings,
+            //        name = "{name}"// recebe {name} pois e o padrao que o servico Consumer reconhece para substituicao para o nome do usuario para o qual ira enviar o email
+            //    };
 
-            var html = await _emailTemplate.GenerateHtmlFromTemplateAsync(NewBookNotifyTemplate, vm);
+            //var html = await _emailTemplate.GenerateHtmlFromTemplateAsync(NewBookNotifyTemplate, vm);
 
-            var message = new AWSSQSMessageNewBookNotifyRequest
-            {
-                Subject = $"Chegou um livro de {book.Category.Name}",
-                BodyHTML = html
-            };
-
-
-            var interestedUsers = _userService.GetBySolicitedBookCategory(book.CategoryId);
+            //var message = new AWSSQSMessageNewBookNotifyRequest
+            //{
+            //    Subject = $"Chegou um livro de {book.Category.Name}",
+            //    BodyHTML = html
+            //};
 
 
-            int maxMessages = interestedUsers.Count() % MAX_DESTINATIONS == 0 ? interestedUsers.Count() / MAX_DESTINATIONS : interestedUsers.Count() / MAX_DESTINATIONS + 1;
+            //var interestedUsers = _userService.GetBySolicitedBookCategory(book.CategoryId);
 
-            for(int i = 1; i <= maxMessages; i++)
-            {
-                var destinations = interestedUsers.Skip((i - 1) * MAX_DESTINATIONS).Take(MAX_DESTINATIONS).Select(u => new DestinationRequest { Name = u.Name, Email = u.Email });
-                message.Destinations = destinations.ToList();
 
-                await _AWSSQSService.SendMessageAsync(message);
-            }
+            //int maxMessages = interestedUsers.Count() % MAX_DESTINATIONS == 0 ? interestedUsers.Count() / MAX_DESTINATIONS : interestedUsers.Count() / MAX_DESTINATIONS + 1;
+
+            //for(int i = 1; i <= maxMessages; i++)
+            //{
+            //    var destinations = interestedUsers.Skip((i - 1) * MAX_DESTINATIONS).Take(MAX_DESTINATIONS).Select(u => new DestinationRequest { Name = u.Name, Email = u.Email });
+            //    message.Destinations = destinations.ToList();
+
+            //    await _AWSSQSService.SendMessageAsync(message);
+            //}
         }
 
         public async Task SendEmailNewBookInserted(Book book)
@@ -137,7 +130,8 @@ namespace ShareBook.Service
             var model = new
             {
                 Book = book,
-                UserStats = userStats
+                UserStats = userStats,
+                UserCreationDateFormated = string.Format("{0:dd/MM/yyyy}", userStats.CreationDate)
             };
 
             var html = await _emailTemplate.GenerateHtmlFromTemplateAsync(NewBookInsertedTemplate, model);
